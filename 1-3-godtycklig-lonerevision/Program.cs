54161a00c5632535214756296bbcfa6b41489e78
js223mx <js223mx@student.lnu.se>
2014-10-02 11:59:30 +0200

    Added methodes

---
+++ 1-3-godtycklig-lonerevision/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_3_godtycklig_lonerevision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfSalaries = 0;
+            numberOfSalaries = ReadInt("Ange antal löner att mata in: ");
+
+
+        }
+
+        static private void ProcessSalaries(int count)
+        {
+
+        }
+
+        static private int ReadInt(string prompt)
+        {
+            
+            string tempovari = null;
+
+            while (true)
+            {
+                Console.Write(prompt);
+
+                tempovari = Console.ReadLine();
+                return int.Parse(tempovari);
+            }
+        }
+
+    }
+}
